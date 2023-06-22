67507f79dc19b899dfc51fabf3f3af78c16fb3a1
humannocturnal <syaputrar374@gmail.com>
2023-06-22 11:10:00 +0700

    Iterasi dari awal array hingga REAR

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -76,7 +76,17 @@ public:
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
 				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
 			}
+
+			FRONT_position = 0;
+
+			//Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
