2e02fe543d412bbec9e2baafe8031b64f6254972
humannocturnal <syaputrar374@gmail.com>
2023-06-22 11:02:20 +0700

    Elements in the queue are

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -65,5 +65,12 @@ public:
 		int REAR_position = REAR;
 
 		// Cek apakah antrian kosong 
+
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElements in the queue are: \n";
 	}
 };
